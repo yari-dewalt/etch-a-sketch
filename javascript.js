38a0d0b7b375f6388e875143a74dd8cd65777c27
yari-dewalt <yari.dewalt@gmail.com>
2023-05-26 14:28:27 -0700

    Add square hover effect

--- javascript.js
+++ javascript.js
@@ -5,3 +5,17 @@ for (let i = 0; i < 16*16; i++) {
     div.setAttribute("id", "square");
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll("#square");
+
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "red";
+    })
+    square.addEventListener("mouseout", () => {
+        square.style.backgroundColor = "white";
+    })
+    square.addEventListener("mousedown", () => {
+        square.style.backgroundColor = "grey";
+    })
+})
