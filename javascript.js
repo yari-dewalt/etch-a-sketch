603e1a4b8d91e5d1c58718b6381db241ae25c525
yari-dewalt <yari.dewalt@gmail.com>
2023-05-26 13:36:52 -0700

    Add 'square' id for each child div

--- javascript.js
+++ javascript.js
@@ -2,5 +2,6 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16*16; i++) {
     let div = document.createElement("div");
+    div.setAttribute("id", "square");
     container.appendChild(div);
 }
